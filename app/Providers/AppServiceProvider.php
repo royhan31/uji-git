3f4e2d9e75b842046c84e95a28104ca7b01c6dad
royhan31 <izzapark34@gmail.com>
2019-06-11 03:36:58 +0700

    new lokerIT

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // 
+        //
     }
 
     /**
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
