9bb880bcd016904dea475e6d1f29c0eeec907aad
royhan31 <izzapark34@gmail.com>
2019-09-24 11:31:24 +0700

    enable cross

--- public/index.php
+++ public/index.php
@@ -8,6 +8,10 @@
  */
 
 define('LARAVEL_START', microtime(true));
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 
 /*
 |--------------------------------------------------------------------------
